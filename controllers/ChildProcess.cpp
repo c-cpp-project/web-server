f8cf9398a42d3832e0191c87a47d68a2685b1d1c
kim sangwon <levelmax14@gmail.com>
2024-02-24 16:36:06 +0900

    cgi infinite loading fix

--- controllers/ChildProcess.cpp
+++ controllers/ChildProcess.cpp
@@ -1,9 +1,9 @@
 #include"ChildProcess.hpp"
 
-std::unordered_map<int, clock_t>	ChildProcess::child;
-double ChildProcess::max_cpu_time_used;
+std::unordered_map<int, timeval>	ChildProcess::child;
+long ChildProcess::max_cpu_time_used;
 
-ChildProcess::ChildProcess(double max_cpu_time_used)
+ChildProcess::ChildProcess(long max_cpu_time_used)
 {
 	child.clear();
 	this->max_cpu_time_used = max_cpu_time_used;
@@ -11,22 +11,15 @@ ChildProcess::ChildProcess(double max_cpu_time_used)
 
 void ChildProcess::insertChildProcess(pid_t pid)
 {
-	child.insert(std::make_pair(pid, clock()));
-}
+	timeval	startTimeval;
 
-clock_t  ChildProcess::getChildProcessClock(pid_t pid)
-{
-	std::unordered_map<int, clock_t>::iterator iter;
-
-	iter = child.find(pid);
-	if (iter == child.end())
-		return (0);
-	return (iter->second);
+	gettimeofday(&startTimeval, NULL);
+	child.insert(std::make_pair(pid, startTimeval));
 }
 
 void ChildProcess::earseChildProcess(pid_t pid)
 {
-	std::unordered_map<int, clock_t>::iterator iter;
+	std::unordered_map<int, timeval>::iterator iter;
 
 	iter = child.find(pid);
 	if (iter != child.end())
@@ -35,27 +28,28 @@ void ChildProcess::earseChildProcess(pid_t pid)
 
 void ChildProcess::waitChildProcess(void)
 {
-	std::unordered_map<int, clock_t>::iterator iter;
-	clock_t	endClock, startClock;
-	double	cpu_time_used;
+	std::unordered_map<int, timeval>::iterator iter;
+	timeval	endTimeval, startTimeval;
+	long	cpu_time_used;
 	pid_t	child_pid;
 	int		status;
 
-	endClock = clock();
+	gettimeofday(&endTimeval, NULL);
 	iter = child.begin();
 	while (iter != child.end())
 	{
 		child_pid = iter->first;
-		startClock = iter->second;
+		startTimeval = iter->second;
 		iter++;
 		if (child.count(child_pid) == 0)
 			continue ;
 		if (waitpid(child_pid, &status, WNOHANG) == 0) // 종료 되지 않았을 경우
 		{
-			cpu_time_used = ((double) (endClock - startClock)) / CLOCKS_PER_SEC;
+			cpu_time_used = (endTimeval.tv_sec - startTimeval.tv_sec) * 1000000 + (endTimeval.tv_usec - startTimeval.tv_usec);
+			cpu_time_used = cpu_time_used / 1000000;
 			if (max_cpu_time_used < cpu_time_used)
 			{
-				std::cout << child_pid << ": EXCEED ALLOCATED CPU TIME " << cpu_time_used << "\n";
+				std::cout << child_pid << ": EXCEED ALLOCATED CPU TIME " << cpu_time_used << "sec\n";
 				kill(child_pid, SIGKILL);
 			}
 		}
--- controllers/ChildProcess.hpp
+++ controllers/ChildProcess.hpp
@@ -2,21 +2,20 @@
 #ifndef CHILD_PROCESS__HPP
 # define CHILD_PROCESS__HPP
 
-#include <ctime>
 #include <unordered_map>
 #include <sys/types.h>
 #include <signal.h>
 #include <iostream>
+#include <sys/time.h>
 
 class   ChildProcess
 {
 private:
-    static std::unordered_map<int, clock_t>	child;
-    static double max_cpu_time_used;
+    static std::unordered_map<int, timeval>	child;
+    static long max_cpu_time_used;
 public:
-    ChildProcess(double max_cpu_time_used);
+    ChildProcess(long max_cpu_time_used);
     static void insertChildProcess(pid_t pid);
-    static clock_t  getChildProcessClock(pid_t pid);
     static void earseChildProcess(pid_t pid);
     static void waitChildProcess(void);
 };

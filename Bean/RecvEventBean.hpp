a3d7369278be5a3273bf36f13fe657eb5e6ad889
Yuijin Kim <yuikim@c6r7s3.42seoul.kr>
2024-02-25 13:30:41 +0900

    feat: RecvEventBean error handling

--- Bean/RecvEventBean.hpp
+++ Bean/RecvEventBean.hpp
@@ -1,6 +1,9 @@
 #ifndef RECV_EVENT_BEAN__HPP
 #define RECV_EVENT_BEAN__HPP
 #include "Bean.hpp"
+#define RECV_COMPLETED 0
+#define RECV_ONGOING -1
+#define RECV_ERROR 1
 
 class Bean;
 class HttpHandler;

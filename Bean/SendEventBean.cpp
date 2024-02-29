9af74cd02c9893695d5a881ab5bb18ebe00a3f33
Yuijin Kim <yuikim@c6r7s3.42seoul.kr>
2024-02-29 17:33:59 +0900

    fix: event

--- Bean/SendEventBean.cpp
+++ Bean/SendEventBean.cpp
@@ -1,39 +1,38 @@
 #include "SendEventBean.hpp"
-#include<sys/socket.h>
+
+#include <sys/socket.h>
 
 SendEventBean::SendEventBean() {}
 SendEventBean::~SendEventBean() {}
 
 #define BUF_SIZE 1024 * 1024
 int SendEventBean::runBeanEvent(HttpHandler *httpHandler, Event *event) {
-	ServerConfiguration 	*serverConfig;
-	std::string				dump;
-	int						socketfd;
-	int						ret;
-	int						buf_size;
-	char					buffer[BUF_SIZE];
+  ServerConfiguration *serverConfig;
+  std::string dump;
+  int socketfd;
+  int ret;
+  int buf_size;
+  char buffer[BUF_SIZE];
 
-	serverConfig = httpHandler->getServerConfiguration();
-	socketfd = httpHandler->getFd();
-	dump = httpHandler->getData();
-	std::cout << serverConfig << " = SendEventBean::runBeanEvent\n";
-	buf_size = BUF_SIZE;
-	if (dump.length() < BUF_SIZE)
-		buf_size = dump.length();
-	std::cout << socketfd << ", " << dump.length() << ": size => " << buf_size << "\n";
-	memcpy(buffer, dump.substr(0, buf_size).c_str(), buf_size);
-	std::cout << buf_size << "\n";
-	ret = write(socketfd, buffer, buf_size);
-	std::cout << ret << " = ret\n";
-	if (ret == 0)
-	{
-		event->saveEvent(socketfd, EVFILT_WRITE, EV_DISABLE, 0, 0, 0);
-		event->saveEvent(socketfd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0,
-										new HttpHandler(socketfd, serverConfig));
-		delete httpHandler;
-		std::cout << "================== SEND DONE ============================\n";
-	}
-	else if (ret > 0)
-		httpHandler->setData(dump.substr(ret));
-	return (ret);
+  serverConfig = httpHandler->getServerConfiguration();
+  socketfd = httpHandler->getFd();
+  dump = httpHandler->getData();
+  std::cout << serverConfig << " = SendEventBean::runBeanEvent\n";
+  buf_size = BUF_SIZE;
+  if (dump.length() < BUF_SIZE) buf_size = dump.length();
+  std::cout << socketfd << ", " << dump.length() << ": size => " << buf_size
+            << "\n";
+  memcpy(buffer, dump.substr(0, buf_size).c_str(), buf_size);
+  std::cout << buf_size << "\n";
+  ret = write(socketfd, buffer, buf_size);
+  std::cout << ret << " = ret\n";
+  if (ret == 0) {
+    event->saveEvent(socketfd, EVFILT_WRITE, EV_DISABLE, 0, 0, 0);
+    event->saveEvent(socketfd, EVFILT_READ, EV_ENABLE, 0, 0,
+                     new HttpHandler(socketfd, serverConfig));
+    delete httpHandler;
+    std::cout << "================== SEND DONE ============================\n";
+  } else if (ret > 0)
+    httpHandler->setData(dump.substr(ret));
+  return (ret);
 }

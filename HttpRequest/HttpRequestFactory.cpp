74591758ac34e267fff04174330a047f649d9410
“rolee” <rolee@student.42seoul.kr>
2024-03-04 08:12:22 +0900

    바디가 너무 긴 경우 throw SocketCloseException413()

--- HttpRequest/HttpRequestFactory.cpp
+++ HttpRequest/HttpRequestFactory.cpp
@@ -42,7 +42,7 @@ long HttpRequestFactory::parseChunkedRequest(HttpRequest* request, ServerConfigu
 	if (end_of_chunk_size == std::string::npos)
 	{
 		if (buffer.size() >= body_size_length + 2)	// "client body size\r\n" 까지 충분히 읽었다면
-			throw SocketCloseException400();		// -> 너무 긴 청크 크기
+			throw SocketCloseException413();		// -> 너무 긴 청크 크기
 		else										// "client body size\r\n" 까지 충분히 읽지 못했다면
 			throw INCOMPLETE_REQUEST;				// -> 불완전한 청크 크기
 	}
